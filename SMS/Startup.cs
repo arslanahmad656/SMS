fed62408583a1667997e31a9b0edd01cc2e19cd7
Usama <Usamayaseen79@gmail.com>
2018-03-27 23:56:11 -0700

    parent role added.Accountant role And user added

--- SMS/Startup.cs
+++ SMS/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using System.Data.Entity;
 using System.Linq;
 using SMS.Models;
+using System;
 
 [assembly: OwinStartupAttribute(typeof(SMS.Startup))]
 namespace SMS
@@ -19,6 +20,66 @@ namespace SMS
         {
             ConfigureAuth(app);
             AddAdminRole();
+            AddParentRole();
+            AddAccountantRole();
+        }
+        private void AddAccountantRole()
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var roleName = "accountant";
+            if (!roleManager.RoleExists(roleName))
+            {
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var role = new IdentityRole
+                {
+                    Name = roleName
+                };
+                var result = roleManager.Create(role);
+                var defaultUserName = "accountant@sms.com";
+                var defaultPassword = "updating";
+                if (result.Succeeded)
+                {
+                    var user = new ApplicationUser
+                    {
+                        UserName = defaultUserName
+                    };
+                    result = userManager.Create(user, defaultPassword);
+                    if (result.Succeeded)
+                    {
+                        result = userManager.AddToRole(user.Id, roleName);
+                        if (!result.Succeeded)
+                        {
+                            throw new System.Exception("Could not add accountant user to admin role");
+                        }
+                    }
+                    else
+                    {
+                        throw new System.Exception("Could not create accountant user.");
+                    }
+                }
+                else
+                {
+                    throw new System.Exception("Could not create accountant role");
+                }
+            }
+        }
+
+        private void AddParentRole()
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var roleName = "parent";
+            if (!roleManager.RoleExists(roleName))
+            {
+                var role = new IdentityRole
+                {
+                    Name = roleName
+                };
+                var result = roleManager.Create(role);
+                if (!result.Succeeded)
+                {
+                    throw new Exception("Could not create Parent role");
+                }
+            }
         }
 
         private void AddAdminRole()
